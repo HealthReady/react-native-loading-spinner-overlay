d3212b23e5c5b1b4809e3e6375fb7f18a7713c5a
HealthReady <mark@healthready.com.au>
2017-03-23 19:01:15 +1100

    Added Android fix for visible prop

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ export default class Spinner extends React.Component {
         onRequestClose={() => this.close()}
         supportedOrientations={['landscape', 'portrait']}
         transparent
-        visible={visible}>
+        visible={visible && <ActivityIndicator />}>
         {spinner}
       </Modal>
     );
